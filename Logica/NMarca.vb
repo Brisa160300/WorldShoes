c624bf0f582d4c331c39bd03b68576683630f31f
Brisa Rios <brisaabigail16@gmail.com>
2022-10-02 18:21:52 -0300

    Actualizar

--- Logica/NMarca.vb
+++ Logica/NMarca.vb
@@ -1,24 +1,24 @@
 ﻿Public Class NMarca
-    Function agregar_marca(descripcion As Integer) As Boolean
-        Dim omarca = New Marcas
-        With omarca
-            .descripcion = descripcion
-            .estado_marca = 1
-        End With
-        Dim objdmarca = New DMarca
-        If objdmarca.Dguardar_marca(omarca) Then
-            Return True
-        Else
-            Return False
-        End If
+    'Function agregar_marca(descripcion As Integer) As Boolean
+    '    Dim omarca = New Marcas
+    '    With omarca
+    '        .descripcion = descripcion
+    '        .estado_marca = 1
+    '    End With
+    '    Dim objdmarca = New DMarca
+    '    If objdmarca.Dguardar_marca(omarca) Then
+    '        Return True
+    '    Else
+    '        Return False
+    '    End If
 
-    End Function
-    Public Sub cargarGrid(grid As DataGridView)
-        Dim mar As DMarca = New DMarca
-        grid.DataSource = marc.getmarcasAll()
-        grid.Columns(0).HeaderText = "ID"
-        grid.Columns(1).HeaderText = "Descripción"
-        grid.Columns(2).HeaderText = "Estado"
-        grid.Columns(3).Visible = False
-    End Sub
+    'End Function
+    'Public Sub cargarGrid(grid As DataGridView)
+    '    Dim mar As DMarca = New DMarca
+    '    grid.DataSource = marc.getmarcasAll()
+    '    grid.Columns(0).HeaderText = "ID"
+    '    grid.Columns(1).HeaderText = "Descripción"
+    '    grid.Columns(2).HeaderText = "Estado"
+    '    grid.Columns(3).Visible = False
+    'End Sub
 End Class
--- Logica/NTalle.vb
+++ Logica/NTalle.vb
@@ -1,26 +1,26 @@
 ﻿Public Class NTalle
-    'Function agregar_talle(descripcion As Integer) As Boolean
-    '    Dim otalle = New talle
-    '    With otalle
-    '        .descripcion = descripcion
-    '        .estado_talle = 1
-    '    End With
-    '    Dim objdtalle = New DTalle
-    '    If objdtalle.Dguardar_talle(otalle) Then
-    '        Return True
-    '    Else
-    '        Return False
-    '    End If
+    Function agregar_talle(descripcion As Integer) As Boolean
+        Dim otalle = New talle
+        With otalle
+            .descripcion = descripcion
+            .estado_talle = 1
+        End With
+        Dim objdtalle = New DTalle
+        If objdtalle.Dguardar_talle(otalle) Then
+            Return True
+        Else
+            Return False
+        End If
 
-    'End Function
-    'Public Sub cargarGrid(grid As DataGridView)
-    '    Dim tal As DTalle = New DTalle
-    '    grid.DataSource = tal.getTallesAll()
-    '    grid.Columns(0).HeaderText = "ID"
-    '    grid.Columns(1).HeaderText = "Descripción"
-    '    grid.Columns(2).HeaderText = "Estado"
-    '    grid.Columns(3).Visible = False
-    'End Sub
+    End Function
+    Public Sub cargarGrid(grid As DataGridView)
+        Dim tal As DTalle = New DTalle
+        grid.DataSource = tal.getTallesAll()
+        grid.Columns(0).HeaderText = "ID"
+        grid.Columns(1).HeaderText = "Descripción"
+        grid.Columns(2).HeaderText = "Estado"
+        grid.Columns(3).Visible = False
+    End Sub
 
 End Class
 
